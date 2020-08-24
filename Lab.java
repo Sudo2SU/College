e6eca0a4177376e259484898782834e047db1a8c
Sudo2SU <41966987+Sudo2SU@users.noreply.github.com>
2020-08-24 17:25:05 +0530

    Add files via upload

---
+++ Lab.java
@@ -0,0 +1,5 @@
+class idelab{  
+    public static void main(String args[]){  
+     System.out.println(" Hello, Welcome to IDE lab -01 (Integrated Development Environment) ");  
+    }  
+}  
---
+++ Lab.py
@@ -0,0 +1,1 @@
+print('Hello welcome to, IDE-Integrated Development Lab!')
